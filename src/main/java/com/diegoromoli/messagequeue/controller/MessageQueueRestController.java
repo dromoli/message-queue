b717ba19cad4c3fa4c4b8fa99c8427832f24c400
Diego Romoli <diego.romoli@intelligentpathways.com.au>
2018-10-02 11:26:53 +1000

    Code cleanup

--- src/main/java/com/diegoromoli/messagequeue/controller/MessageQueueRestController.java
+++ src/main/java/com/diegoromoli/messagequeue/controller/MessageQueueRestController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +26,4 @@ public class MessageQueueRestController {
         Message message = messageService.storeMessage(messageString);
         return message.getId();
     }
-
 }
--- src/test/java/com/diegoromoli/messagequeue/MessageQueueApplicationTests.java
+++ src/test/java/com/diegoromoli/messagequeue/MessageQueueApplicationTests.java
@@ -1,19 +1,13 @@
 package com.diegoromoli.messagequeue;
 
 import com.diegoromoli.messagequeue.controller.MessageQueueRestController;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -42,5 +36,4 @@ public class MessageQueueApplicationTests extends AbstractMessageQueueTest {
         String content = mvcResult.getResponse().getContentAsString();
         assertEquals(1L, Long.parseLong(content));
     }
-
 }
